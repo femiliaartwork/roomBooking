bbf6c7b4fd022f55da47311cf9441edb40f8bb2d
femiliaartwork <131574021+femiliaartwork@users.noreply.github.com>
2023-04-26 17:59:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="./css/fontawesome-all.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="./css/styles.css" />
+    <script src="./js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <title>Room Booking System</title>
+
+
+</head>
+
+<body>
+    <h2 class="">Room Booking System</h2>
+    <div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="margin-bottom:10px;">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="container">
+        <div class="available room">Available</div>
+        <div class="unavailable room">Unavailable</div>
+        <div class="reserved room">Reserved</div>
+        <div class="my-reservation room">My Reservation</div>
+    </div>
+
+    <php?>
+        </php>
+</body>
+
+<footer>
+    © Copyright Team Fruit- 2023
+</footer>
+
+</html>
